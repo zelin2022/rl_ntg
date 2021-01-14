1ab22a00f622d6a017816bbf5f93aeb60507e06e
Zelin Liu <zelinliu@hotmail.com>
2021-01-14 02:39:45 -0700

    fixed json tag for MatchRecord

--- environment/channelstructs/struct_MatchRecord.go
+++ environment/channelstructs/struct_MatchRecord.go
@@ -7,12 +7,12 @@ import (
 )
 
 type MatchRecord struct {
-  Players []string `json="players"`
-  StartTime int64 `json="start_time"`
-  EndTime int64 `json="end_time"`
-  Winner string `json="winner"`
-  WinReason string `json="win_reason"`
-  Moves []string `json="moves"`
+  Players []string `json:"players"`
+  StartTime int64 `json:"start_time"`
+  EndTime int64 `json:"end_time"`
+  Winner string `json:"winner"`
+  WinReason string `json:"win_reason"`
+  Moves []string `json:"moves"`
 }
 
 func (mr *MatchRecord) ToString()string{
