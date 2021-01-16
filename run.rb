c7b2766a45dd51a6f6ee7d859e1e330354f4150c
Zelin Liu <zelinliu@hotmail.com>
2021-01-16 05:04:46 -0700

    RL-28 consolidate pathing in run.rb

--- run.rb
+++ run.rb
@@ -13,32 +13,34 @@ end
 
 if __FILE__ == $0
   execute_time = Time.new.strftime("%Y-%m-%d-%H:%M:%S:%L")
-  Dir.mkdir("log") unless Dir.exist?"log" #DEBUG
-  # Dir.mkdir("log/#{execute_time}")
-  Dir.mkdir("log/current") unless Dir.exist? "log/current"
-  # `mkdir log/#{execute_time}`
+
+  log_dir = File.join(__dir__, 'log')
+  log_current_dir = File.join(log_dir, 'current')
+  FileUtils.mkdir_p(log_dir) unless File.directory?(log_dir)
+  FileUtils.mkdir_p(log_current_dir) unless File.directory?(log_current_dir)
 
   # clean up record
   record_dir = File.join(__dir__, 'record')
-  old_record_dir = File.join(record_dir, 'old_record')
-  FileUtils.mkdir_p(old_record_dir) unless File.directory?(old_record_dir)
+  record_old_record_dir = File.join(record_dir, 'old_record')
+  FileUtils.mkdir_p(record_dir) unless File.directory?(record_dir)
+  FileUtils.mkdir_p(record_old_record_dir) unless File.directory?(record_old_record_dir)
   Dir.new(record_dir).each {|file|
     file = File.join(record_dir, file)
-    FileUtils.mv(file, old_record_dir) if File.file? file
+    FileUtils.mv(file, record_old_record_dir) if File.file? file
   }
 
   # start env
   #env_id = spawn "go run environment/main.go > log/#{execute_time}/environment.log 2>&1"
-  env_id = spawn "go run environment/main.go > log/current/environment.log 2>&1" #DEBUG change to one folder for easy of debugging
+  env_id = spawn "go run environment/main.go > #{log_current_dir}/environment.log 2>&1" #DEBUG change to one folder for easy of debugging
 
   # wait a bit
-  sleep 5
+  sleep 2
 
   # start agents
   agents_id = []
   for i in (1..ARGV[0].to_i)
     #agents_id.append(spawn("python3 agent/main.py > log/#{execute_time}/agent#{i}.log 2>&1"))
-    agents_id.append(spawn("python3 agent/main.py #{i} > log/current/agent#{i}.log 2>&1")) #DEBUG
+    agents_id.append(spawn("python3 agent/main.py #{i} > #{log_current_dir}/agent#{i}.log 2>&1")) #DEBUG
   end
 
   # at_exit{
